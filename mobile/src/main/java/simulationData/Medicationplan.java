ff9972a6cf577466ee20502d558b202d45fa1238
vonkc2 <corinavonkaenel@gmail.com>
2016-01-14 08:55:10 +0100

    Darstellung Tagesvariable Verschreibung - Mediplan item

--- mobile/src/main/java/simulationData/Medicationplan.java
+++ mobile/src/main/java/simulationData/Medicationplan.java
@@ -34,7 +34,7 @@ public class Medicationplan {
         mMediplan.add(new Medication(1002, "Durogesic Matrix TTS", "100mcg/h", "Fentanyl", "Pflaster", "xInterval", "days, 3", "28.12.15", "weeks, 3", "Praxis Dr. Matt, Biel", "alle 3 Tage"));
         mMediplan.add(new Medication(1003, "Vesicare", "5mg", "Solifenacin succinat", "Tablette", "standard", "nachts, 1 Tablette", "01.10.15", "months, 6", "Praxis Dr. Matt, Biel", "nachts: 1 Tablette"));
         mMediplan.add(new Medication(1004, "Norflocin-Mepha ", "400mg", "Norfloxacin", "Tablette", "standard", "mittags|nachts, 1 Tablette", "04.01.16", "weeks, 2", "Praxis Dr. Matt, Biel", "mittags und nachts: 1 Tablette"));
-        mMediplan.add(new Medication(1005, "Marcoumar", "3mg", "Phenpro coumon", "Tablette", "tagesvariabel", "mo,1 1/4|3/4|1|3/4|3/4|1|3/4", "01.10.15", "months, 6", "Praxis Dr. Matt, Biel", "Mo 1 1/4,Di 3/4,Mi 1,Do 3/4,Fr 3/4,Sa 1,So 3/4"));
+        mMediplan.add(new Medication(1005, "Marcoumar", "3mg", "Phenpro coumon", "Tablette", "tagesvariabel", "mo,1 1/4|3/4|1|3/4|3/4|1|3/4", "01.10.15", "months, 6", "Praxis Dr. Matt, Biel", "heute 1 1/4 Tablette (Sa 3/4,So 1)"));
 
     }
 
